812923b449f68bcf7939afe5fe718768f3834477
Sh0KingFE <kbednarczyk@interia.com>
2023-03-16 20:33:34 +0100

    Lista-zakupow

---
+++ script.js
@@ -0,0 +1,27 @@
+const zakupy = document.getElementsByClassName("zakupy")[0];
+const pencil = document.getElementById("pencil");
+const allItems = document.getElementById("allItems");
+const userInput = document.getElementById("input");
+
+pencil.addEventListener('click', function(){
+    allItems.innerHTML = "";
+});
+
+userInput.addEventListener('keydown', function(event){
+    if(event.key == "Enter")
+    addItem();
+    
+})
+
+function addItem(){
+    var h2 = document.createElement("h2");
+    h2.innerHTML = "- " + userInput.value;
+
+    h2.addEventListener("click", function(){
+        h2.style.textDecoration = "line-through";
+    })
+
+    allItems.insertAdjacentElement("beforeend", h2);
+
+    userInput.value = "";
+}
